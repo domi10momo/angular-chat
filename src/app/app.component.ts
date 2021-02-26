6fd975439bdfd1b5c266eff55f621e86366085e5
hisadomi <hb.kmaia93@icloud.com>
2021-02-26 23:31:56 +0900

    comment用クラス、データを作成、表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  { name: '武井　賢治', message: 'おつかれさまです！' },
+  { name: '武井　賢治', message: 'この間の件ですが。。。' },
+  { name: '五十嵐 洋平', message: 'おつかれさまです！' },
+];
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
 }
